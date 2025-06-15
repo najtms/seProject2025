b0cc1eb65841d11cefaf3c58bf6f935790d08e9a
vildannk <vildan.kadric@stu.ibu.edu.ba>
2025-06-15 20:25:14 +0200

    Add Config.php properly with correct casing

---
+++ backend/dao/Config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASS', getenv('DB_PASS'));
+define('DB_PORT', getenv('DB_PORT'));
+
